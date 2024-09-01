a703f6e18e566400dce28821a99f99ebf672770e
Grecia Osorio <you@example.com>
2024-08-31 23:08:35 -0500

    fix test page

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -15,120 +15,28 @@ void main() {
       driver?.close();
     }
   });
-  group('Happy Paths', () {
-    /*
-      Given I am on the Coffee Device Selection Screen
-      When I tap "French Press"
-      And I tap "Continue"
-      And I enter "5"
-      And I tap "Continue"
-      Then I should see "63g - course ground coffee"
-      And I should see "887g - water"
-    */
-    test("should give recommendation for French Press", () {
-      // your code here
-    },skip:true);
 
-    /*
-      Given I am on the Coffee Device Selection Screen
-      When I tap "Drip Machine"
-      And I tap "Continue"
-      And I enter "5"
-      And I tap "Continue"
-      Then I should see "52g - medium ground coffee"
-      And I should see "887g - water"
-    */
-    test("should give recommendation for Drip Machine", () {
-      //your code here
-    }, skip:true);
+  group('Happy Paths', () {
+    test("should be able to sign in", () async {
+      print('Tapping "Sign Up" button');
+      await driver?.tap(find.text('Sign Up'));
+      await Future.delayed(const Duration(seconds: 1)); // Add
+      // your code here
+    });
+
+
   });
 
-  group('Sad Paths', () {
-    /*
-      Given I am on the Coffee Device Selection Screen
-      When I press "Continue"
-      Then I expect to still be on the Coffee Device Selection Screen
-    */
-    test("should not advance from Choose Device Screen without a selection",
-        () {
-      //your code here
-    },skip:true);
-
-    /*
-      Given I chose "French Press" on the Coffee Device Selection Screen
-      And I advanced to the Choose Cups Screen
-      When I press "Continue"
-      Then I expect to still be on the Choose Cups Screen
-    */
-    test("should not advance from Choose Cups Screen without cups", () {
-      //your code here
-    },skip:true);
-
-    /*
-      Given I chose "French Press" on the Coffee Device Selection Screen
-      And I advanced to the Choose Cups Screen
-      When I enter "-1"
-      And I press "Continue"
-      Then I expect to still be on the Choose Cups Screen
-    */
-    test("should not advance from Choose Cups Screen with negative cup amount",
-        () {
-      //your code here
-    },skip:true);
-
-    /*
-      Given I chose "French Press" on the Coffee Device Selection Screen
-      And I advanced to the Choose Cups Screen
-      When I enter "a"
-      And I press "Continue"
-      Then I expect to still be on the Choose Cups Screen
-    */
-    test(
-        "should not advance from Choose Cups Screen with letter for cup amount",
-        () {
-      //your code here
-      //if you can restrict the keyboard to numbers only you can delete this test
-    },skip:true);
-
-    /*
-      Given I chose "Drip Machine" on the Coffee Device Selection Screen
-      And I advanced to the Choose Cups Screen
-      When I press "Continue"
-      Then I expect to still be on the Choose Cups Screen
-    */
-    test("should not advance from Choose Cups Screen without cups", () {
-      //your code here
-    },skip:true);
-
-    /*
-      Given I chose "Drip Machine" on the Coffee Device Selection Screen
-      And I advanced to the Choose Cups Screen
-      When I enter "-1"
-      And I press "Continue"
-      Then I expect to still be on the Choose Cups Screen
-    */
-    test("should not advance from Choose Cups Screen with negative cup amount",
-        () {
-      //your code here
-    },skip:true);
-
-    /*
-      Given I chose "Drip Machine" on the Coffee Device Selection Screen
-      And I advanced to the Choose Cups Screen
-      When I enter "a"
-      And I press "Continue"
-      Then I expect to still be on the Choose Cups Screen
-    */
-    test(
-        "should not advance from Choose Cups Screen with letter for cup amount",
-        () {
-      //your code here
-      //if you can restrict the keyboard to numbers only you can delete this test
-    });
-  },skip:true);
-
-  group('Back Button', () {
-    //make up your own tests to check that the back button works
-    //on every page
-  },skip:true);
+  // group('Sad Paths', () {
+  //   /*
+  //     Given I am on the Coffee Device Selection Screen
+  //     When I press "Continue"
+  //     Then I expect to still be on the Coffee Device Selection Screen
+  //   */
+  //   test("should not advance from Choose Device Screen without a selection",
+  //       () {
+  //     //your code here
+  //   },skip:true);
+  //   //on every page
+  // },skip:true);
 }
