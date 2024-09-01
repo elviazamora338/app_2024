9cfa5bcf3ed664e986f23ce7d949c634213245cd
elviazamora338 <elviazamora338@gmail.com>
2024-08-31 20:11:52 -0500

    login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,14 +20,21 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
         backgroundColor: const Color(0xFFF3ACB5),
       ), 
-      backgroundColor: Color(0xFF500F15),
-      body: const Center(
+      backgroundColor: const Color(0xFF500F15),
+      // my body
+      body:  Center(
+        // creating a column
         child: Column(
+          // making sure it's in the center of screen
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start, // Aligns children to the left
+          // children of the column
           children: 
           [
-            // const SizedBox(height: 80),
-            Text("Login",
+            // the text title is login
+            const Text("Login",
+            // this was not working >:( 
+              // textAlign: TextAlign.left,
               style: TextStyle(
                 color: Color(0xFFFCD7DD),
                 fontSize: 20,
@@ -35,7 +42,70 @@ class _LoginScreenState extends State<LoginScreen> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            
+            // some spacing
+            const SizedBox(height: 20),
+            // a container for the textfields
+            Container(
+              width: 350,
+              height: 150,
+              // some decoration
+              decoration: BoxDecoration(
+                color: const Color(0xFFF3ACB5),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: const Color(0xFFFCD7DD),
+                  width: 2,
+                ),
+                // chaning the color of the shadow
+                // boxShadow: const [
+                //   BoxShadow(
+                //     color: Color(0xFFFCD7DD),
+                //     blurRadius: 10,
+                //     offset: Offset(0, 5),
+                //   ),
+                // ],                
+              ),
+              // the children of the container
+              child: const Column(
+                children: [
+                  // the listtile for the username
+                  ListTile(
+                    title: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Enter your username",
+                        hintStyle: TextStyle(
+                          color: Color(0xFF500F15),
+                          fontSize: 18,
+                          fontFamily: "Montserrat-Regular",
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  // divider between username and password
+                  Divider(
+                    color: Color(0xFFFCD7DD),
+                    thickness: 2,
+                  ),
+                  // the listtile for the password
+                  ListTile(
+                    title: TextField(
+                      // hides password
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter your password",
+                        hintStyle: TextStyle(
+                          color: Color(0xFF500F15),
+                          fontSize: 18,
+                          fontFamily: "Montserrat-Regular",
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
